a96cb63b5ce5ddbf103c12fb69807f4a40c71381
liwen <liwen1010322212@163.com>
2019-09-21 13:19:22 +0800

    添加ide_helper 方便注释

--- app/Http/Modules/User.php
+++ app/Http/Modules/User.php
@@ -4,6 +4,30 @@
 namespace App\Http\Modules;
 
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Http\Modules\User
+ *
+ * @property int $id
+ * @property string $name 用户名
+ * @property string $password 密码
+ * @property string $salt 盐值
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereSalt($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class User extends BaseModel
 {
 
