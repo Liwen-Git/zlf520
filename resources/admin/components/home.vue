373efdc2aded948a2c7a8ea3a49b09afb7dc57f6
liwen <liwen1010322212@163.com>
2019-09-24 20:50:32 +0800

    home 菜单

--- resources/admin/components/home.vue
+++ resources/admin/components/home.vue
@@ -1,7 +1,38 @@
 <template>
-    <div>
-        <router-view></router-view>
-    </div>
+    <el-row class="public-height-hundred">
+        <el-col :span="18" :offset="3" class="public-height-hundred">
+            <el-container class="public-height-hundred">
+                <el-header>
+                    <el-menu
+                            :default-active="activeMenu"
+                            router
+                            mode="horizontal"
+                            background-color="#545c64"
+                            text-color="#fff"
+                            active-text-color="#ffd04b">
+                        <el-menu-item index="/welcome"><i class="el-icon-s-home"></i>主页</el-menu-item>
+                        <el-submenu index="2">
+                            <template slot="title">我的工作台</template>
+                            <el-menu-item index="2-1">选项1</el-menu-item>
+                            <el-menu-item index="2-2">选项2</el-menu-item>
+                            <el-menu-item index="2-3">选项3</el-menu-item>
+                            <el-submenu index="2-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                                <el-menu-item index="2-4-3">选项3</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                    </el-menu>
+                </el-header>
+                <el-main>
+                    <el-card shadow="hover">
+                        <router-view></router-view>
+                    </el-card>
+                </el-main>
+            </el-container>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
@@ -10,7 +41,7 @@
     export default {
         data() {
             return {
-
+                activeMenu: '/welcome',
             }
         },
         computed: {
@@ -35,3 +66,9 @@
         }
     }
 </script>
+
+<style scoped>
+    .public-height-hundred {
+        height: 100%;
+    }
+</style>
--- resources/admin/components/welcome.vue
+++ resources/admin/components/welcome.vue
@@ -1,9 +1,11 @@
 <template>
 	<el-row>
 		<el-col :span="8"><div class="grid-content "></div></el-col>
-		<el-col :span="8"><div class="grid-content ">
-			<h1 >zlf</h1>
-		</div></el-col>
+		<el-col :span="8">
+			<div class="grid-content ">
+				<h1 >zlf</h1>
+			</div>
+		</el-col>
 		<el-col :span="8"><div class="grid-content "></div></el-col>
 	</el-row>
 </template>
@@ -17,7 +19,7 @@
 		text-align: center;
 		border-radius: 4px;
 		min-height: 36px;
-		margin-top: 150px;
+		margin: 150px 0;
 	}
 	h1{
 		font-size: 34px;
