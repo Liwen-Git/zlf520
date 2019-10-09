bb39c05d57796ebfe3770f3007ab3a131d1c520b
liwen <liwen1010322212@163.com>
2019-10-09 15:39:37 +0800

    微信关注

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -9,8 +9,22 @@ class WeChatController extends Controller
     public function serve()
     {
         $app = app('wechat.official_account');
-        $app->server->push(function () {
-            return "欢迎关注WEN";
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'text':
+                    return "很开心认识你~~~";
+                    break;
+                case 'event':
+                    if ($message['Event'] == 'subscribe') {
+                        return '欢迎关注多乐多乐!!!';
+                    } else {
+                        return "ヽ(￣▽￣)و";
+                    }
+                    break;
+                default:
+                    return "^_^";
+                    break;
+            }
         });
 
         return $app->server->serve();
