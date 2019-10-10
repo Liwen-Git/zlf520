e92fbd1ca6fb4fb1e9d2796a1291bef24a094c25
liwen <liwen1010322212@163.com>
2019-10-10 11:06:22 +0800

    web hook test

--- public/webhook.php
+++ public/webhook.php
@@ -15,3 +15,5 @@ $res = shell_exec('cd /root/www/zlf520/ && git pull 2>&1');
 $res_log = '-------------------------'.PHP_EOL;
 $res_log .= ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push'.$res;
 file_put_contents("git-webhook.txt", $res_log, FILE_APPEND);
+
+// 测试
