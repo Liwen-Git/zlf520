9ea62b095a8d9826dd3c2e197a5e841d8279a24a
liwen <liwen1010322212@163.com>
2019-10-10 10:50:14 +0800

    web hook input

--- public/webhook.php
+++ public/webhook.php
@@ -1,7 +1,14 @@
 <?php
 
 // 获取push数据内容的方法
-$request = file_get_contents("php://input");
+$requestBody = file_get_contents("php://input");    //接收数据
+if (empty($requestBody)) {              //判断数据是不是空
+    die('send fail');
+}
+$content = json_decode($requestBody, true);     //数据转换
 
 // 只需这一行代码便可拉取
-shell_exec('cd /root/www/zlf520 && git pull');
+$res = shell_exec('cd /root/www/zlf520/ && git pull 2>&1');
+$res_log = '-------------------------'.PHP_EOL;
+$res_log .= ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push'.$res;
+file_put_contents("git-webhook.txt", $res_log, FILE_APPEND);//将每次拉取信息追加写入到日志里
